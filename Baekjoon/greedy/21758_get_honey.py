597bf44f53d07f6b7cc4b3777b7a77e56293683e
HBell <sleehj96@naver.com>
2021-08-16 12:24:26 +0900

    21758 solved

--- Baekjoon/greedy/21758_get_honey.py
+++ Baekjoon/greedy/21758_get_honey.py
@@ -1,27 +1,28 @@
 # https://www.acmicpc.net/problem/21758
 
-# 55점
-
 import sys
 
-N = int(input())
+# sys.stdin = open('input.txt')
+N = int(sys.stdin.readline())
 
 # spots = list(map(int, input().split()))
 spots = tuple(map(int, sys.stdin.readline().split()))
 # print(spots)
 
+honey_total = sum(spots)
+
 # 2 가지 케이스를 생각
 
 # 1) 벌 - 꿀통 - 벌
 
-# honey_sum1 = sum(spots[1:-1]) + max(spots)
-max_spot = spots[0]
-honey_sum1 = 0
-for idx in range(1, N - 1):
-    if max_spot < spots[idx]:
-        max_spot = spots[idx]
-    honey_sum1 += spots[idx]
-honey_sum1 += max_spot
+# max_spot = spots[0]
+# honey_sum1 = 0
+# for idx in range(1, N - 1):
+#     if max_spot < spots[idx]:
+#         max_spot = spots[idx]
+#     honey_sum1 += spots[idx]
+# honey_sum1 += max_spot
+honey_sum1 = honey_total - spots[0] - spots[-1] + max(spots)
 
 
 # 2) 벌 - 벌 - 꿀통
@@ -34,18 +35,27 @@ honey_sum1 += max_spot
 #     if sum(spots[:mid_spot]) > sum(spots[mid_spot+1:]):  # 정 중앙의 꿀을 제외하고 비교
 #         spots.reverse
 
-honey_spot_left = bee_a_right = len(spots) - 1
+honey_spot_left = bee_a_right = N - 1
 bee_a_left = honey_spot_right = 0
 honey_sum_left = honey_sum_right = 0
 
-for bee_b in range(1, honey_spot_left):
-    bee_a_honey_left = sum(spots[1:]) - spots[bee_b]
-    bee_b_honey_left = sum(spots[bee_b + 1:])
-    bee_total_honey_left = bee_a_honey_left + bee_b_honey_left
+bee_b_passed_left = spots[0]
+bee_b_passed_right = spots[-1]
 
-    bee_a_honey_right = sum(spots[:-1]) - spots[bee_b]
-    bee_b_honey_right = sum(spots[:bee_b])
-    bee_total_honey_right = bee_a_honey_right + bee_b_honey_right
+for bee_b in range(1, honey_spot_left):
+    # bee_a_honey_left = sum(spots[1:]) - spots[bee_b]
+    # bee_b_honey_left = sum(spots[bee_b + 1:])
+    # bee_total_honey_left = bee_a_honey_left + bee_b_honey_left
+    bee_b_passed_left += spots[bee_b]
+    bee_total_honey_left = 2 * honey_total - \
+        spots[0] - spots[bee_b] - bee_b_passed_left
+
+    # bee_a_honey_right = sum(spots[:-1]) - spots[bee_b]
+    # bee_b_honey_right = sum(spots[:bee_b])
+    # bee_total_honey_right = bee_a_honey_right + bee_b_honey_right
+    bee_b_passed_right += spots[- 1 - bee_b]
+    bee_total_honey_right = 2 * honey_total - \
+        spots[-1] - spots[-1-bee_b] - bee_b_passed_right
 
     if honey_sum_left < bee_total_honey_left:
         honey_sum_left = bee_total_honey_left
