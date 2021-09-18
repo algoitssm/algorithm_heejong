693509189a58de8a4a139f7ce880004a2de3e937
HBell <sleehj96@naver.com>
2021-09-18 11:22:40 +0900

    9489 solved

--- Baekjoon/tree/9489_cousins.py
+++ Baekjoon/tree/9489_cousins.py
@@ -1,18 +1,44 @@
 # https://www.acmicpc.net/problem/9489
+# Google Helped me
+
+from collections import defaultdict
+
 
 while True:
     n, k = map(int, input().split())
 
-    if not n and not k:
+    if not n:
         break
 
     nodes = list(map(int, input().split()))
 
-    parent = [[] for _ in range(n+1)]
-    childs = [[] for _ in range(n+1)]
+    # if k == nodes[0]:
+    #     print(0)
+    #     continue
 
-    for node in nodes:
-        pass
+    G, level = defaultdict(list), {}
+    cur_p = 0   # nodes에서 현재 index
+    G[0], level[nodes[cur_p]] = [nodes[cur_p]], 0  # G는 트리, level는 노드별 레벨 저장
+    # print(dict(G), level)
+
+    for i in range(1, n):
+        # print('cur_p:', cur_p)
+        # print('nodes[cur_p]:', nodes[cur_p])
+        # print(G[nodes[cur_p]])
+        if G[nodes[cur_p]] != []:
+            if nodes[i] != G[nodes[cur_p]][-1] + 1:
+                cur_p += 1
+        G[nodes[cur_p]].append(nodes[i])
+        level[nodes[i]] = nodes[cur_p]
+        # print(dict(G), level)
+
+    ans = 0
+    for p in G[level[level[k]]]:
+        if p != level[k]:
+            ans += len(G[p])
+
+    print(ans)
+    # break
 
     # tmp = nodes[0]
     # G = []
@@ -32,7 +58,18 @@ while True:
     #     G.append(tmp_list)
 
     # G.pop(0)
-    # print(G)
+    # G.pop(0)
+
+    # tree = [[] for _ in range(max(nodes)+1)]
+
+    # idx = 0
+    # level = []
+    # for childs in G:
+    #     print(nodes[idx])
+    #     tree[nodes[idx]] = childs
+    #     idx += 1
+
+    # print(tree)
 
     # while idx < len(G):
 
