b5ce3437f2a4f582338bb8593bcef0cd3c1589a9
HBell <sleehj96@naver.com>
2021-09-28 23:19:07 +0900

    1952 solved

--- SWEA/1952_3.py
+++ SWEA/1952_3.py
@@ -2,24 +2,6 @@ import sys
 sys.stdin = open('input.txt')
 
 
-# def calc(cost, m):  # (이전 달까지의 계산 결과, 현재 내가 보낼 결과)
-#     global min_cost
-
-#     if m > 12:
-#         if min_cost > cost:
-#             min_cost = cost
-#         return
-
-#     # # 1일권
-#     # calc(cost + d * month[m], m + 1)
-#     # # 1달권
-#     # calc(cost + m1, m + 1)
-#     calc(cost + min(d*month[m], m1), m+1)
-
-#     # 3달권
-#     calc(cost + m3, m + 3)
-
-
 for tc in range(int(input())):
     d, m1, m3, y = map(int, input().split())    # [일, 달, 3달, 년]
     month = [0] + list(map(int, input().split()))
--- SWEA/1952_swimming_pool.py
+++ SWEA/1952_swimming_pool.py
@@ -1,44 +1,31 @@
 import sys
 sys.stdin = open('input.txt')
 
+
+def check(n, my_sum):
+    global min_sum
+
+    if n >= 12:
+        if min_sum > my_sum:
+            min_sum = my_sum
+        return
+
+    check(n + 1, my_sum + min(prices[0]*plans[n+1], prices[1]))
+
+    check(n + 3, my_sum + prices[2])
+
+
 for tc in range(int(input())):
     prices = list(map(int, input().split()))    # [일, 달, 3달, 년]
-    plans = list(map(int, input().split()))
+    plans = [0] + list(map(int, input().split()))
     # month_pay_check = [0 for _ in range(12)]
 
-    month_pay = [0 for _ in range(12)]
+    # month_pay = [0 for _ in range(12+1)]
 
-    days = months = 0
-    mark = -1
-    # month_in_row = 0
-    for idx, plan in enumerate(plans):
-        if idx == mark:
-            if sum(month_pay[idx-2:idx+1]) > prices[2]:
-                month_pay[idx-2] = 0
-                month_pay[idx-1] = 0
-                month_pay[idx] = prices[2]
-            mark = -1
-            pass
-
-        if plan:
-            if plan >= prices[1]//prices[0]:
-                month_pay[idx] = prices[1]
-                # month_pay_check[idx] = 1
-                mark = idx+2
-                if mark > 12:
-                    mark = 11
-
-                # month_in_row += 1
-
-            else:
-                month_pay[idx] = plan * prices[0]
-
-    print(month_pay)
-    ans = sum(month_pay)
-    if ans > prices[3]:
-        ans = prices[3]
+    min_sum = prices[3]
+    check(0, 0)     # n, sum
 
     # total_swimming_days = sum(plans)
 
-    print('#{0} {1}'.format(tc+1, ans))
+    print('#{0} {1}'.format(tc+1, min_sum))
     # break
