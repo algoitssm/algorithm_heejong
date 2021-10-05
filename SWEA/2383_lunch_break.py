9dcf8348ff17a78e7ab9281ed2b5deda1b80a328
HBell <sleehj96@naver.com>
2021-10-05 21:11:36 +0900

    2477 solved

--- SWEA/2383_lunch_break.py
+++ SWEA/2383_lunch_break.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 sys.stdin = open('input12.txt')
 
 
@@ -7,30 +6,34 @@ def distance(v, w):
     return abs(v[0] - w[0]) + abs(v[1] - w[1])
 
 
-# 원래 재귀로 구현했어야 하는데 케이스가 적어서 생각해본 부끄러운 풀이네요..
 def time_to_take(lst: list, length: int):
     lst.sort()
 
-    if len(lst) <= 3:
-        return lst[-1] + length + 1
+    t_sum = 0
+    for idx in range((len(lst)-1) % 3, len(lst)-1, 3):      # 하위 로직을 일반화
+        t_sum += lst[idx] + length
+    return max(t_sum, lst[-1]) + length + 1
 
-    elif len(lst) == 4:
-        return max(lst[0] + length, lst[-1]) + length + 1
+    # if len(lst) <= 3:
+    #     return lst[-1] + length + 1
 
-    elif len(lst) == 5:
-        return max(lst[1] + length, lst[-1]) + length + 1
+    # elif len(lst) == 4:
+    #     return max(lst[0] + length, lst[-1]) + length + 1
 
-    elif len(lst) == 6:
-        return max(lst[2] + length, lst[-1]) + length + 1
+    # elif len(lst) == 5:
+    #     return max(lst[1] + length, lst[-1]) + length + 1
 
-    elif len(lst) == 7:
-        return max(lst[0] + length + lst[3] + length, lst[-1]) + length + 1
+    # elif len(lst) == 6:
+    #     return max(lst[2] + length, lst[-1]) + length + 1
 
-    elif len(lst) == 8:
-        return max(lst[1] + length + lst[4] + length, lst[-1]) + length + 1
+    # elif len(lst) == 7:
+    #     return max(lst[0] + length + lst[3] + length, lst[-1]) + length + 1
 
-    elif len(lst) == 9:
-        return max(lst[2] + length + lst[5] + length, lst[-1]) + length + 1
+    # elif len(lst) == 8:
+    #     return max(lst[1] + length + lst[4] + length, lst[-1]) + length + 1
+
+    # elif len(lst) == 9:
+    #     return max(lst[2] + length + lst[5] + length, lst[-1]) + length + 1
 
 
 for tc in range(int(input())):
@@ -43,29 +46,31 @@ for tc in range(int(input())):
     persons = []
     for row in range(N):
         for col in range(N):
-            if G[row][col] == 1:
+            if G[row][col] == 1:            # 사람
                 persons.append((row, col))
-            elif G[row][col] >= 2:
+            elif G[row][col] >= 2:          # 계단
                 stairs.append((row, col))
 
     ans = 987654321
-    for binaried in range(1 << len(persons)):
-        case = bin(binaried)[2:].zfill(len(persons))
+    for binaried in range(1 << len(persons)):           # 경우의 수 = 2^(사람수)
+        case = bin(binaried)[2:].zfill(len(persons))    # 이진형태로 표현하고 시작
 
+        # [0번째 stair, 1번째 stair]   ex) [[4,2], [4,3,3,2]]
         distance_list = [[], []]
         for idx in range(len(persons)):
             case_idx = int(case[idx])
             distance_list[case_idx].append(
-                distance(persons[idx], stairs[case_idx]))
+                distance(persons[idx], stairs[case_idx]))   # 걸리는 시간별로 리스트화되어 저장
 
-        times = [0, 0]
+        times = [0, 0]          # 걸리는 시간 저장 [0번째 계단, 1번째 계단]
         for idx2 in range(2):
             if distance_list[idx2] != []:
-                stair_time = G[stairs[idx2][0]][stairs[idx2][1]]
-                times[idx2] = time_to_take(distance_list[idx2], stair_time)
+                stair_time = G[stairs[idx2][0]][stairs[idx2][1]]    # 계단 시간
+                times[idx2] = time_to_take(
+                    distance_list[idx2], stair_time)  # 계단당 걸리는 시간을 times에 저장
 
-        if ans > max(times):
-            ans = max(times)
+        if ans > max(times):    # 각 계단의 시간 중 가장 오래 걸리는 시간
+            ans = max(times)    # 최소인 경우 갱신
 
     print('#{} {}'.format(tc+1, ans))
     # break
---
+++ SWEA/2477_car_center.py
@@ -0,0 +1,93 @@
+import sys
+sys.stdin = open('input13.txt')
+
+
+for tc in range(int(input())):
+
+    # 접수창구, 정비창구, 고객수, 접수 창구번호, 정비 창구번호
+    N, M, K, A, B = map(int, input().split())
+
+    # 접수 창구 = A, 정비 창구 = B
+    a_list = list(map(int, input().split()))        # A 창구별 걸리는 시간
+    b_list = list(map(int, input().split()))        # B 창구별 걸리는 시간
+    t_list = list(map(int, input().split()))        # 손님 도착 시간 (정렬된 형태로 주어지는듯?)
+
+    customer_visit = [[0, 0] for _ in range(K)]     # 고객 K명의 이용한 창구 번호 저장
+
+    a_boxes = [[] for _ in range(len(a_list))]      # A 창구별 소요시간
+    a_waiting_queue = []                            # A 창구 대기열
+
+    b_boxes = [[] for _ in range(len(b_list))]      # B 창구별 소요시간
+    b_waiting_queue = []                            # B 창구 대기열
+
+    finished = [0 for _ in range(K)]            # 손님이 정비까지 끝냈는지 확인 (for 반복문 종료)
+
+    time = 0
+    while True:
+        if all(finished):                           # 모두 정비를 끝 마쳤으면 종료
+            break
+
+        # print('time: ', time)
+
+        for person_idx, t in enumerate(t_list):     # t_list를 순회하면서
+            if t == time:                           # 해당시간의 사람을
+                a_waiting_queue.append(person_idx)  # A 창구 대기열에 추가
+
+        # A
+        for idx in range(len(a_boxes)):             # 접수 창구를 확인
+
+            if a_boxes[idx] != []:                  # 창구에 사람이 있을 때
+                if a_boxes[idx][1] > 0:             # 아직 시간이 남았으면
+                    a_boxes[idx][1] -= 1            # 하나씩 빼줌
+
+                if a_boxes[idx][1] == 0:            # 시간이 다 되었으면
+                    person_idx = a_boxes[idx][0]    # 몇번째 사람인지 보고
+                    a_boxes[idx] = []               # 그 창구를 비우고
+                    b_waiting_queue.append(person_idx)  # B 창구 대기열에 그 사람 추가
+
+            if a_boxes[idx] == []:                          # 창구가 비었으면
+                if a_waiting_queue:                         # A 대기열에 사람이 있으면
+                    person_idx = a_waiting_queue.pop(0)     # 제일 처음 온 사람을
+                    a_boxes[idx] = [person_idx, a_list[idx]]    # 그 창구에 투입
+                    customer_visit[person_idx][0] = idx     # 해당 손님 A 창구번호 저장
+
+        # B
+        for idx in range(len(b_boxes)):             # 정비 창구 확인
+
+            if b_boxes[idx] != []:                  # 창구에 사람이 있을 때
+                if b_boxes[idx][1] > 0:             # 아직 시간이 남아있으면
+                    b_boxes[idx][1] -= 1            # 하나씩 빼줌
+
+                if b_boxes[idx][1] == 0:            # 시간이 다 되었으면
+                    person_idx = b_boxes[idx][0]    # 몇번째 사람인지 보고
+                    b_boxes[idx] = []               # 그 창구를 비우고
+                    finished[person_idx] = 1        # 그 사람 완료 처리
+
+            if b_boxes[idx] == []:                  # 창구가 비었으면
+                if b_waiting_queue:                 # B 대기열에 사람이 있으면
+                    person_idx = b_waiting_queue.pop(0)         # 제일 처음 온 사람을
+                    b_boxes[idx] = [person_idx, b_list[idx]]    # 그 창구에 투입
+                    customer_visit[person_idx][1] = idx     # 해당 손님 B 창구번호 저장
+
+        # print('a_queue: ', a_waiting_queue)
+        # print('a_boxes: ', a_boxes)
+
+        # print('b_queue: ', b_waiting_queue)
+        # print('b_boxes: ', b_boxes)
+
+        # print('customer_visit: ', customer_visit)
+        # print('-------')
+        time += 1
+
+    # print(customer_visit)
+
+    ans = 0
+    for idx in range(K):        # 손님 전체 순회
+        if customer_visit[idx] == [A-1, B-1]:   # 방문 창구번호가 같으면
+            ans += idx + 1      # 그 손님 번호를 누적
+
+    if ans == 0:                # 아무도 없으면
+        ans = -1                # -1 출력
+
+    print('#{} {}'.format(tc+1, ans))
+    # break
